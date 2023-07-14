397cb6298be8103157c4e6b620615ca245f7884b
akarlov <akarlov@credos.ru>
2023-07-14 16:42:43 +0300

    14072023 [Rx]

--- aeon.AEOHSolution/aeon.AEOHSolution.ClientBase/Company/CompanyActions.cs
+++ aeon.AEOHSolution/aeon.AEOHSolution.ClientBase/Company/CompanyActions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -7,4 +7,34 @@ using aeon.AEOHSolution.Company;
 
 namespace aeon.AEOHSolution.Client
 {
+  partial class CompanyActions
+  {
+    public virtual void CreateNOR(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      if ( aeon.AEOHSolution.PublicFunctions.Company.NORNotFound(_obj))
+      {
+        try{
+          aeon.AEOHSolution.PublicFunctions.Company.SCreateNOR(_obj);
+          e.AddInformation("Наша организация создана");
+        }
+        catch(Exception ex)
+        {
+          e.AddError("Вознилка ошибка: " + ex.Message);
+        }
+        
+      }
+      
+      else e.AddInformation("По данному контрагенту уже заведена наша организация");
+      
+     
+    }
+
+    public virtual bool CanCreateNOR(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      if (_obj.State.IsInserted) return false;
+      else return true;
+    }
+
+  }
+
 }
--- aeon.AEOHSolution/aeon.AEOHSolution.Server/Company/CompanyServerFunctions.cs
+++ aeon.AEOHSolution/aeon.AEOHSolution.Server/Company/CompanyServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,32 @@ namespace aeon.AEOHSolution.Server
 {
   partial class CompanyFunctions
   {
+
+    [Public]
+    public bool NORNotFound()
+    {
+      return !aeon.AEOHSolution.BusinessUnits.GetAll().Any(x=>(x.TIN==_obj.TIN && x.TRRC==_obj.TRRC) || (x.TIN==_obj.TIN));
+    }
+    /// <summary>
+    /// Создать НОР
+    /// </summary>
+    [Public]
+    public void SCreateNOR()
+    {
+        //Закрываем старого контрагента:
+        _obj.Status = Sungero.CoreEntities.DatabookEntry.Status.Closed;
+        _obj.Save();
+        //Создаем новую НОР:
+        var NOR =  aeon.AEOHSolution.BusinessUnits.Create();
+        NOR.Name = _obj.Name;
+        NOR.LegalName = _obj.LegalName;
+        NOR.Region = _obj.Region;
+        NOR.TIN = _obj.TIN;
+        NOR.TRRC = _obj.TRRC;
+        //NOR.Company = _obj;
+        NOR.Save();
+      
+    }
 
   }
 }
